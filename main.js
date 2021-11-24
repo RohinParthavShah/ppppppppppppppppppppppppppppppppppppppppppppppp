7b8c66bd0bd90b846b4f5d4a58a344aaef7b11ee
RohinParthavShah <83176943+RohinParthavShah@users.noreply.github.com>
2021-11-24 09:33:37 -0600

    Update main.js

--- main.js
+++ main.js
@@ -33,26 +33,26 @@ function my_keydown(e)
 	
 		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90))
 		{
-			aplhabetkey()
+			aplhabetkey();
 			document.getElementById("d1").innerHTML="You pressed alphabet key";
 			console.log("aplhabetKey");
 	
 		}
 	if(keyPressed >=48 && keyPressed<=57)
 		{
-			numberkey()
+			numberkey();
 			document.getElementById("d1").innerHTML="You  pressed number  key";
 			console.log("numberKey");
 	
 		}
-	if((keyPressed >=37 && keyPressed<=40)
-			arrowkey()
+	if(keyPressed >=37 && keyPressed<=40)
+			arrowkey();
 			document.getElementById("d1").innerHTML="You presse arrow key";
 			console.log("arrowKey");
 	
-		}if(keyPressed = 17 || keyPressed =18  ||   keyPressed = 27) 
+		}if(keyPressed == 17 || keyPressed == 18  ||   keyPressed == 27) 
 		{
-			specialkey()
+			specialkey();
 			document.getElementById("d1").innerHTML="You pressed special key";
 			console.log("specialkey");
 	
